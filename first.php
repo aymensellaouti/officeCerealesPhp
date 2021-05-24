d66bdbea9b6c478923d8911a91f246e1e51fec44
aymen2782 <aymen.sellaouti@gmail.com>
2021-05-24 11:26:09 +0100

    ajout first.php test php in html

---
+++ first.php
@@ -0,0 +1,26 @@
+<?php
+$name = "aymen";
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+            echo "Bonjour $name";
+        ?>
+    </p>
+    <div>
+        <?php
+            echo 'Bonjour $name<br>';
+            echo "Bonjour {$name}<br>";
+        ?>
+    </div>
+</body>
+</html>
